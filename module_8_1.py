e799ddea10e8f1d4fadb41996f85ad5257b76f55
re-indweller <asf.ayrat@gmail.com>
2024-10-18 20:45:32 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+# Домашнее задание по теме "Try и Except"
+# Задание "Программистам всё можно":
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
